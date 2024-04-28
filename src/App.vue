836a4eef4f9c4a68e00d545dbcb140f4d520679a
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-04-28 22:05:03 +0300

    transactions moved to global state & sent and received via props

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,22 @@
   <div class="container">
     <Balance />
     <IncomeExpenses />
-    <TransactionList />
+    <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
 </template>
-<script>
+<script setup>
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
 
-export default {
-  components: {
-    Header,
-    Balance,
-    TransactionList,
-    IncomeExpenses,
-    AddTransaction,
-  },
-};
+import { ref } from "vue";
+const transactions = ref([
+  { id: 1, text: "Flower", amount: -19.99 },
+  { id: 2, text: "Salary", amount: 299.97 },
+  { id: 3, text: "Book", amount: -10 },
+  { id: 4, text: "Camera", amount: 150 },
+]);
 </script>
